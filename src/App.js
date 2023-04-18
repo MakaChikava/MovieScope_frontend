52227d04bd439c0bc1e83d1c9a3f767ae7d1889b
makachikava <chikavamakanaka@gmail.com>
2023-04-18 14:25:10 -0700

    changed package.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbar from './components/navbar';
-
 import './App.css';
+
 const App = () => {
   let emptyMovie={title:'', overview:'', poster:''}
   let [movies, setMovies] = useState([]);
